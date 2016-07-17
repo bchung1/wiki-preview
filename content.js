69ce4b5b0fbb3a64ff82c4199243843eba7cfaf0
Brian Chung <bchung1@umd.edu>
2016-07-17 15:07:15 -0400

    initial commit tooltip functionality done

---
+++ content.js
@@ -0,0 +1,34 @@
+$('a').hoverIntent({
+	over: function(event) { 
+		var href = event.target.href; 
+		var text = $('body').find("p:first").text(); 
+		getHrefHtml(href, this); 
+	}, 
+	out: function(){
+
+	},
+	interval: 400
+})
+
+
+function getHrefHtml(href, currLink){
+	$.ajax({ 
+		url: href, 
+		success: function(html) { 
+			getFirstParagraph(html, currLink); 
+		} 
+	});
+}
+
+
+function getFirstParagraph(htmlText, currLink){
+	var  html = $.parseHTML(htmlText);
+	var  first_bolded_word = $(html).find('#mw-content-text p > b:first');
+	var first_para = $(first_bolded_word).parent().text();
+	$(currLink).attr({
+		'data-balloon-length': 'xlarge',
+		'data-balloon': first_para,
+		'data-balloon-pos': 'up'
+	});
+
+}
